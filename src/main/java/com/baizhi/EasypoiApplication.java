8924829739a700569987fbf8f2c7da69e86e350d
chufena <54537478+chufena@users.noreply.github.com>
2019-08-27 14:31:57 +0800

    Update EasypoiApplication.java

--- src/main/java/com/baizhi/EasypoiApplication.java
+++ src/main/java/com/baizhi/EasypoiApplication.java
@@ -9,6 +9,7 @@ public class EasypoiApplication {
     public static void main(String[] args) {
         SpringApplication.run(EasypoiApplication.class, args);
         System.out.println("是我啊");
+        System.out.println("是ni啊");
     }
 
 }
